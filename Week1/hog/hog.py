81cebd08713e0afec1da1166cc578c91346bf11b
liquidhiter <909494727@qq.com>
2022-11-05 21:39:51 +0100

    Added other files

--- Week1/hog/hog.py
+++ Week1/hog/hog.py
@@ -389,7 +389,7 @@ def final_strategy(score, opponent_score):
     *** YOUR DESCRIPTION HERE ***
     """
     # BEGIN PROBLEM 12
-    
+    return 6  # Replace this statement
     # END PROBLEM 12
 
 ##########################
